4b7d97c282794458b9c9c00baa5c32682d171811
vikafrostb10 <vikafrostb10@gmail.com>
2019-11-28 19:59:24 +0200

    Added scss file

--- modalWindow.js
+++ modalWindow.js
@@ -1,16 +1,16 @@
-$(document).on("click", ".naccs .menu div", function() {
+$(document).on("click", ".products .menu div", function() {
     var numberIndex = $(this).index();
 
     if (!$(this).is("active")) {
-        $(".naccs .menu div").removeClass("active");
-        $(".naccs ul li").removeClass("active");
+        $(".products .menu div").removeClass("active");
+        $(".products ul li").removeClass("active");
 
         $(this).addClass("active");
-        $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+        $(".products ul").find("li:eq(" + numberIndex + ")").addClass("active");
 
-        var listItemHeight = $(".naccs ul")
+        var listItemHeight = $(".nacproductscs ul")
             .find("li:eq(" + numberIndex + ")")
             .innerHeight();
-        $(".naccs ul").height(listItemHeight + "px");
+        $(".oroducts ul").height(listItemHeight + "px");
     }
 });
